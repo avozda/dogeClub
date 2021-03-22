c15928bb3b05d36cbfd6fef6ff1dae88b7d585bf
Adam Vozda <adamvozda88@seznam.cz>
2021-03-22 14:16:38 +0100

    commit on "master"

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User")
@@ -46,7 +48,24 @@ router.post("/", [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
         // jwt
-        res.send("User registered");
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get("jwtSecret"), { expiresIn: 3600 },
+            (err, token) => {
+                if (err) throw err;
+
+                res.json({ token });
+
+            }
+        )
+
     } catch (error) {
         console.error(error);
         res.status(500).send("server error")
